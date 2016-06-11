4c710140e5fbef7c0c59647a14cb3f5740b90daa
WheetTweet <crescentblue1122@gmail.com>
2016-06-11 09:35:13 +0900

    ButtonClickSimulator.cs : Just sample for emulating button click of uGUI.
    AutoScanController.cs : clicking button at 4 sec interval.

---
+++ AutoScanController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace StructureAR
+{
+
+public class AutoScanController : MonoBehaviour {
+
+		[SerializeField] Button button;
+		private float timeOut=4.0f;
+		private float timeElapsed;
+
+		void Update(){
+			timeElapsed += Time.deltaTime;
+
+			if(timeElapsed >= timeOut) {
+				generateFrameAndDisappear ();
+
+				timeElapsed = 0.0f;
+			}
+		}
+
+		private void generateFrameAndDisappear(){
+			button.onClick.Invoke ();
+		}
+
+}
+}
---
+++ ButtonClickSimulator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonClickSimulator : MonoBehaviour {
+
+	[SerializeField] Button button;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.E)) {
+			button.onClick.Invoke ();
+		}
+	}
+
+
+}
